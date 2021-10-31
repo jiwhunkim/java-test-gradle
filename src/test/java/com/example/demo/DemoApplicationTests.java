15c6ba2588ed0341f8064008891e75ca9ddb9f49
Jiwhun Kim <jiwhun.kim@outlook.com>
2021-10-31 11:41:06 +0900

    assertJ dependency add

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,11 +1,13 @@
 package com.example.demo;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        Assertions.assertThat(1).isEqualTo(1);
+    }
 
 }
