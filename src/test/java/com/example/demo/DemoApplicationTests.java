8dff323ddc905a6bf5bfd5d92365b8a1e343098b
Jiwhun kim <jiwhun.kim@outlook.com>
2025-02-16 17:31:59 +0900

    java version change

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,14 @@
 package com.example.demo;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class DemoApplicationTests {
 
     @Test
     void contextLoads() {
-        Assertions.assertThat(1).isEqualTo(1);
+        System.out.println("hello");
+        Assertions.assertEquals("1", "1");
     }
 
 }
